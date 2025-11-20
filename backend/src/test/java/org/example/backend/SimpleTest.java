4f2ef9e731a0d319af7cd260c6abb3eb2235c6f5
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2025-11-20 19:45:12 +0200

    added JUnit

---
+++ backend/src/test/java/org/example/backend/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.example.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleTest {
+
+    @Test
+    void shouldPass() {
+        assertEquals(2, 1 + 1);
+    }
+}
