f641ac02142b131874fc88bafd3d3d44032f597f
adham165 <adhamanas2004@gmail.com>
2025-11-27 07:04:48 +0200

    add org in requests

--- backend/src/main/java/org/example/backend/requests/Requests.java
+++ backend/src/main/java/org/example/backend/requests/Requests.java
@@ -1,6 +1,7 @@
 package org.example.backend.requests;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import org.example.backend.admin.Admin;
@@ -45,5 +46,9 @@ public class Requests {
         if (createdAt == null) {
             createdAt = LocalDateTime.now();
         }
+    }
+    @JsonProperty("organization")
+    public String getOrganizationName() {
+        return organization != null ? organization.getName() : null;
     }
 }
