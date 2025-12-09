b5da7004516a8ccabd324d68cc5a30a7b994b1f0
NourrKhalled <nourrkhalled123@gmail.com>
2025-12-09 05:46:36 +0200

    Added PostCard Component

---
+++ frontend/src/components/PostCard.jsx
@@ -0,0 +1,120 @@
+import { useState, useEffect, useRef, useContext } from 'react';
+import { IoIosPerson } from "react-icons/io";
+import { BsThreeDots } from "react-icons/bs";
+import { BiUpvote, BiDownvote, BiSolidUpvote, BiSolidDownvote } from "react-icons/bi";
+import { RiShareForwardLine } from "react-icons/ri";
+import { FaRegComment } from "react-icons/fa";
+import "./style/postCard.css";
+import { AuthContext } from '../context/AuthContext';
+
+const PostCard = ({ postBody = {} }) => {
+    const [userVote, setUserVote] = useState(0);
+    const [voteCount, setVoteCount] = useState(postBody.votes || 0);
+    const [postOptions, setPostOptions] = useState(false);
+    const [viewerMenu, setViewerMenu] = useState([
+        { label: "Follow", onClick: () => console.log("Follow clicked") }
+    ]);
+
+    const [authorMenu, setAuthorMenu] = useState([
+        { label: "Edit", onClick: () => console.log("Edit clicked") },
+        { label: "Delete", onClick: () => console.log("Delete clicked") }
+    ]);
+
+    
+    const { user } = useContext(AuthContext);
+    
+    const menuRef = useRef(null);
+
+    const handleVote = (voteType) => {
+        const previousVote = userVote;
+        const newVote = userVote === voteType ? 0 : voteType;
+        
+        const voteDifference = newVote - previousVote;
+        
+        setUserVote(newVote);
+        setVoteCount(prevCount => prevCount + voteDifference);
+    };
+
+    const handleOptions = () => {
+        setPostOptions(!postOptions);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setPostOptions(false);
+            }
+        };
+
+        if (postOptions) {
+            document.addEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [postOptions]);
+
+    return(
+        <article className="post-card">
+            <div className="post-header">
+                <div className="user-profile-pic">
+                    {postBody.avatar ? postBody.avatar : <IoIosPerson />}
+                </div>
+                <div className="user-info">
+                    <h2 className="user-name">{postBody.firstName} {postBody.lastName}</h2>
+                    <time datetime="2024-12-09 30:00">{postBody.time}</time>
+                </div>
+                <div className="post-settings" ref={menuRef}>
+                    <BsThreeDots onClick={() => setPostOptions(prev => !prev)}/>
+                        {postOptions && (
+                            <div className="options-menu">
+                            <ul>
+                            {(postBody.userId === user.id ? authorMenu : viewerMenu).map((item, index) => (
+                                <li key={index} onClick={item.onClick}>{item.label}</li>
+                            ))}
+                            </ul>
+                        </div>
+                        )}
+                </div>
+            </div>
+            <div className="post-content">
+                <div className="post-title">
+                    <p>{postBody.title}</p>
+                </div>
+                <div className="post-media">
+                    {postBody.media ?
+                    (<img src={postBody.media} alt={postBody.title || "Post content"} />)
+                    : 
+                    (null)
+                    }
+                    
+                </div>
+            </div>
+            <footer className="post-footer">
+                <div className="up-down-vote">
+                    {userVote === 1 ? (
+                        <BiSolidUpvote className="selected" onClick={() => handleVote(1)} />
+                    ) : (
+                        <BiUpvote onClick={() => handleVote(1)} />
+                    )}
+                    <span className="vote-count">{voteCount}</span>
+                    <span className="vote-separator">|</span>
+                    {userVote === -1 ? (
+                        <BiSolidDownvote className="selected" onClick={() => handleVote(-1)} />
+                    ) : (
+                        <BiDownvote onClick={() => handleVote(-1)} />
+                    )}
+                </div>
+                <div className="post-comment">
+                    <FaRegComment />
+                </div>
+                <div className="post-share">
+                    <RiShareForwardLine />
+                </div>
+            </footer>
+        </article>
+    );
+};
+
+export default PostCard;
