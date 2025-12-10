6128de969e3fa3a673dd05fd01ba8ee49055e64d
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2025-12-10 20:48:45 +0200

    get followed forums

--- backend/src/main/java/org/example/backend/forumfollowing/FollowingRepository.java
+++ backend/src/main/java/org/example/backend/forumfollowing/FollowingRepository.java
@@ -1,4 +1,6 @@
 package org.example.backend.forumfollowing;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import org.bson.types.ObjectId;
 import org.example.backend.forum.Forum;
@@ -12,4 +14,6 @@ public interface FollowingRepository extends MongoRepository<Following, ObjectId
     List<Following> findByUserId(ObjectId userId);
     boolean existsByUserIdAndForumId(ObjectId userId, ObjectId forumId);
     void deleteByUserIdAndForumId(ObjectId userId, ObjectId forumId);
+
+    Page<Following> findByUserId(ObjectId userId, Pageable pageable);
 }
