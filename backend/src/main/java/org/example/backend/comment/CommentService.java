fa791a4cd1e07bd5fa750d3db061235107d751d6
adham165 <adhamanas2004@gmail.com>
2025-12-15 10:53:42 +0200

    handle create at in add comments

--- backend/src/main/java/org/example/backend/comment/CommentService.java
+++ backend/src/main/java/org/example/backend/comment/CommentService.java
@@ -74,6 +74,7 @@ public class CommentService {
                 .postId(postId)
                 .parentId(parentId)
                 .content(addCommentDTO.getContent())
+                .createdAt(Instant.now())
                 .build();
         return comment;
     }
