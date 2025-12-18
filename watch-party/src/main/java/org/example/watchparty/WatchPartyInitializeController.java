a0109632ae09f5bbf421117c67195a30f12ae6bf
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2025-12-18 22:37:32 +0200

    init watch-party service

---
+++ watch-party/src/main/java/org/example/watchparty/WatchPartyInitializeController.java
@@ -0,0 +1,10 @@
+package org.example.watchparty;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/watch-party")
+public class WatchPartyInitializeController {
+        
+}
