5758965f8a6b22b69b7bf534addb5ac897138e41
MohamedElturky <153658210+MohamedElturky@users.noreply.github.com>
2025-12-23 23:52:42 +0200

    update user-api

--- frontend/src/api/user-api.jsx
+++ frontend/src/api/user-api.jsx
@@ -9,6 +9,33 @@ export async function getUserProfileApi({ userId }) {
     }
 }
 
+export async function isUserFollowedApi({ userId }) {
+    try {
+        const response = await api.get(`/follow/v1/is-followed/${userId}`);
+        return { success: true, data: response.data === true };
+    } catch (err) {
+        return { success: false, message: err.message };
+    }
+}
+
+export async function followUserApi({ userId }) {
+    try {
+        const response = await api.post(`/follow/v1/follow/${userId}`);
+        return { success: true, data: response.data };
+    } catch (err) {
+        return { success: false, message: err.message };
+    }
+}
+
+export async function unfollowUserApi({ userId }) {
+    try {
+        const response = await api.post(`/follow/v1/unfollow/${userId}`);
+        return { success: true, data: response.data };
+    } catch (err) {
+        return { success: false, message: err.message };
+    }
+}
+
 export async function getIsPublicApi() {
     try {
         const response = await api.get(`/user/v1/is-public`);
