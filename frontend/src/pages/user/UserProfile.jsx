8d8aa1a7547f17112225a4da228fe7d93fbb266d
MohamedElturky <153658210+MohamedElturky@users.noreply.github.com>
2025-12-24 21:40:41 +0200

    resolving conflict

--- frontend/src/pages/user/UserProfile.jsx
+++ frontend/src/pages/user/UserProfile.jsx
@@ -5,7 +5,7 @@ import { IoIosPerson } from 'react-icons/io';
 import { FaUserPlus, FaUserCheck } from 'react-icons/fa';
 import { ToastContext } from '../../context/ToastContext.jsx';
 import { getModApi } from '../../api/forum-api.jsx';
-import { getUserProfileApi } from '../../api/user-api.jsx';
+import { getUserProfileApi, isUserFollowedApi, followUserApi, unfollowUserApi } from '../../api/user-api.jsx';
 import WatchLaterPanel from '../../components/WatchLaterPanel.jsx';
 import { formatCount } from '../../utils/formate.jsx';
 import './style/UserProfile.css';
